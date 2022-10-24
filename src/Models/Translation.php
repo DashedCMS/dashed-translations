e403d1f6d858bc8905a9fce422f182860a2b0cf4
Robin van Maasakker <robin@qubiqx.com>
2022-10-24 14:38:17 +0200

    Update

--- src/Models/Translation.php
+++ src/Models/Translation.php
@@ -2,13 +2,13 @@
 
 namespace Qubiqx\QcommerceTranslations\Models;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Str;
 use Spatie\Activitylog\LogOptions;
-use Spatie\Activitylog\Traits\LogsActivity;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Database\Eloquent\Model;
 use Spatie\Translatable\HasTranslations;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Translation extends Model
 {
