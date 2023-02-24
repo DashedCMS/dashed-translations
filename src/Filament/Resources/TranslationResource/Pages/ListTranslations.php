293759d1739448d9bd1f77b2cd54354e937fc0f6
Robin van Maasakker <robin@qubiqx.com>
2023-02-24 07:55:58 +0100

    Update

--- src/Filament/Resources/TranslationResource/Pages/ListTranslations.php
+++ src/Filament/Resources/TranslationResource/Pages/ListTranslations.php
@@ -2,25 +2,25 @@
 
 namespace Qubiqx\QcommerceTranslations\Filament\Resources\TranslationResource\Pages;
 
-use Carbon\Carbon;
 use Closure;
-use Filament\Forms\Components\DateTimePicker;
-use Filament\Forms\Components\FileUpload;
-use Filament\Forms\Components\Repeater;
-use Filament\Forms\Components\Section;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+use Filament\Resources\Pages\Page;
 use Filament\Forms\Components\Tabs;
+use Illuminate\Support\Facades\Cache;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Contracts\HasForms;
+use FilamentTiptapEditor\TiptapEditor;
+use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Tabs\Tab;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Concerns\InteractsWithForms;
-use Filament\Forms\Contracts\HasForms;
-use Filament\Resources\Pages\Page;
-use FilamentTiptapEditor\TiptapEditor;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Str;
+use Filament\Forms\Components\FileUpload;
 use Qubiqx\QcommerceCore\Classes\Locales;
-use Qubiqx\QcommerceTranslations\Filament\Resources\TranslationResource;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Concerns\InteractsWithForms;
 use Qubiqx\QcommerceTranslations\Models\Translation;
+use Qubiqx\QcommerceTranslations\Filament\Resources\TranslationResource;
 
 class ListTranslations extends Page implements HasForms
 {
@@ -118,6 +118,7 @@ class ListTranslations extends Page implements HasForms
                         $schema[] = FileUpload::make("translation_{$translation->id}_{$locale['id']}")
                             ->disk('public')
                             ->default($translation->default)
+                            ->enableDownload()
                             ->label(Str::of($translation->name)->replace('_', ' ')->replace('-', ' ')->title())
                             ->helperText($helperText ?? '');
                     } elseif ($translation->type == 'repeater') {
