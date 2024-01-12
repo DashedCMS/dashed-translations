f66f9aef0258a3635ae23335c00e5459fd545ebb
Robinvm <Robinvm@users.noreply.github.com>
2024-01-12 15:05:04 +0000

    Fix styling

--- src/Models/Translation.php
+++ src/Models/Translation.php
@@ -45,9 +45,9 @@ class Translation extends Model
     public static function get($name, $tag, $default = null, $type = 'text', $variables = null)
     {
         //Cannot use this because this fails emails etc
-//        if (app()->runningInConsole()) {
-//            return $default;
-//        }
+        //        if (app()->runningInConsole()) {
+        //            return $default;
+        //        }
 
         if ($name && $default === null) {
             $default = $name;
