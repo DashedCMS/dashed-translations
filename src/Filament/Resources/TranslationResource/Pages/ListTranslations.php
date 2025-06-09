46ac56bf5106a4ab985ffac1a1c217c560edf9c0
Robin van Maasakker <robin@qubiqx.com>
2025-06-09 14:40:28 +0200

    Update

--- src/Filament/Resources/TranslationResource/Pages/ListTranslations.php
+++ src/Filament/Resources/TranslationResource/Pages/ListTranslations.php
@@ -85,10 +85,14 @@ class ListTranslations extends Page
                 foreach ($translations as $translation) {
                     $helperText = '';
                     if ($translation->variables && is_array($translation->variables)) {
-                        $helperText = 'Beschikbare variablen: <br>';
+                        $helperText = 'Beschikbare variablen: ';
 
                         foreach ($translation->variables as $key => $value) {
-                            $helperText .= ":$key: (bijv: $value) <br>";
+                            $helperText .= ":$key: (bijv: $value)";
+
+                            if ($key != array_key_last($translation->variables)) {
+                                $helperText .= ', ';
+                            }
                         }
                     }
 
@@ -357,10 +361,10 @@ class ListTranslations extends Page
                         if ($bool === true) {
                             $translation = Translation::find($id);
                             StartTranslationOfModel::dispatch($translation, $data['from_locale'], $data['to_locales']);
-                            //                            $textToTranslate = $translation->getTranslation('value', $data['from_locale']) ?: $translation->default;
-                            //                            foreach ($data['to_locales'] as $locale) {
-                            //                                TranslateValueFromModel::dispatch($translation, 'value', $textToTranslate, $locale, $data['from_locale']);
-                            //                            }
+//                            $textToTranslate = $translation->getTranslation('value', $data['from_locale']) ?: $translation->default;
+//                            foreach ($data['to_locales'] as $locale) {
+//                                TranslateValueFromModel::dispatch($translation, 'value', $textToTranslate, $locale, $data['from_locale']);
+//                            }
                         }
                     }
 
@@ -415,11 +419,11 @@ class ListTranslations extends Page
                         if ($bool === true) {
                             $translations = Translation::where('tag', $tab)->get();
                             foreach ($translations as $translation) {
-                                //                                $textToTranslate = $translation->getTranslation('value', $data['from_locale']) ?: $translation->default;
-                                //                                foreach ($data['to_locales'] as $locale) {
+//                                $textToTranslate = $translation->getTranslation('value', $data['from_locale']) ?: $translation->default;
+//                                foreach ($data['to_locales'] as $locale) {
                                 StartTranslationOfModel::dispatch($translation, $data['from_locale'], $data['to_locales']);
-                                //                                    TranslateValueFromModel::dispatch($translation, 'value', $textToTranslate, $locale, $data['from_locale']);
-                                //                                }
+//                                    TranslateValueFromModel::dispatch($translation, 'value', $textToTranslate, $locale, $data['from_locale']);
+//                                }
                             }
                         }
                     }
@@ -451,11 +455,11 @@ class ListTranslations extends Page
                 ->action(function (array $data, $livewire) use ($locale) {
                     $id = explode('_', $livewire->mountedFormComponentActionsComponents[0])[1];
                     $translation = Translation::find($id);
-                    //                    $textToTranslate = $translation->getTranslation('value', $locale['id']) ?: $translation->default;
+//                    $textToTranslate = $translation->getTranslation('value', $locale['id']) ?: $translation->default;
                     StartTranslationOfModel::dispatch($translation, $data['locales'], $locale['id']);
-                    //                    foreach ($data['locales'] as $otherLocale) {
-                    //                        TranslateValueFromModel::dispatch($translation, 'value', $textToTranslate, $otherLocale, $locale['id']);
-                    //                    }
+//                    foreach ($data['locales'] as $otherLocale) {
+//                        TranslateValueFromModel::dispatch($translation, 'value', $textToTranslate, $otherLocale, $locale['id']);
+//                    }
 
                     Notification::make()
                         ->title(Str::of($translation->name)->replace('_', ' ')->replace('-', ' ')->title() . " wordt vertaald")
